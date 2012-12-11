f67697f658cd189751986ca1864661c60de8d25a
Filip.Ryjacek@gmail.com <Filip.Ryjacek@gmail.com@b833036d-153d-82ad-6c32-ff65e0eae3d7>
2012-12-11 12:29:52 +0000

    ActiveDocument properties, UI project

---
+++ trunk/v100319/SQL2008_BIDSHelper/SSRS/ReportParametersPlugin.cs
@@ -0,0 +1,147 @@
+using System;
+using Extensibility;
+using EnvDTE;
+using EnvDTE80;
+using System.Xml;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Data;
+
+namespace BIDSHelper.SSRS
+{
+    public class ReportParametersPlugin : BIDSHelperPluginBase
+    {
+        public ReportParametersPlugin(Connect con, DTE2 appObject, AddIn addinInstance)
+            : base(con, appObject, addinInstance)
+        {
+        }
+
+        public override string ShortName
+        {
+            get { return "ReportParameters"; }
+        }
+
+        public override int Bitmap
+        {
+            get { return 543; }
+        }
+
+        public override string ButtonText
+        {
+            get { return "Report Parameters Details..."; }
+        }
+
+        public override string ToolTip
+        {
+            get { return "my Tooltip"; } //not used anywhere
+        }
+
+        public override bool ShouldPositionAtEnd
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// Gets the name of the friendly name of the plug-in.
+        /// </summary>
+        /// <value>The friendly name.</value>
+        public override string FeatureName
+        {
+            get { return "Unused Report Datasets"; }
+        }
+
+        /// <summary>
+        /// Gets the Url of the online help page for this plug-in.
+        /// </summary>
+        /// <value>The help page Url.</value>
+        public override string  HelpUrl
+        {
+            get { return this.GetCodePlexHelpUrl("Report Parameters Details"); }
+        }
+
+        /// <summary>
+        /// Gets the feature category used to organise the plug-in in the enabled features list.
+        /// </summary>
+        /// <value>The feature category.</value>
+        public override BIDSFeatureCategories FeatureCategory
+        {
+            get { return BIDSFeatureCategories.SSRS; }
+        }
+
+        /// <summary>
+        /// Gets the full description used for the features options dialog.
+        /// </summary>
+        /// <value>The description.</value>
+        public override string FeatureDescription
+        {
+            get { return "Provides a overview of report parameters."; }
+        }
+        
+        private string[] SSRS_FILE_EXTENSIONS = { ".rdl" };
+
+        /// <summary>
+        /// Determines if the command should be displayed or not.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public override bool DisplayCommand(UIHierarchyItem item)
+        {
+            try
+            {
+                UIHierarchy solExplorer = this.ApplicationObject.ToolWindows.SolutionExplorer;
+                if (((System.Array)solExplorer.SelectedItems).Length != 1)
+                    return false;
+
+                UIHierarchyItem hierItem = ((UIHierarchyItem)((System.Array)solExplorer.SelectedItems).GetValue(0));
+                string sFileName = ((ProjectItem)hierItem.Object).Name.ToLower();
+                foreach (string extension in SSRS_FILE_EXTENSIONS)
+                {
+                    if (sFileName.EndsWith(extension))
+                        return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public override void Exec()
+        {
+            try
+            {
+
+            UIHierarchy solExplorer = this.ApplicationObject.ToolWindows.SolutionExplorer;
+            UIHierarchyItem hierItem = (UIHierarchyItem)((System.Array)solExplorer.SelectedItems).GetValue(0);
+            ProjectItem projItem = (ProjectItem)hierItem.Object;
+
+            string sFileName = ((ProjectItem)hierItem.Object).Name.ToLower();
+
+            MessageBox.Show(sFileName);
+            
+            if (this.ApplicationObject.ActiveDocument != null)
+            {
+                Form1 f = new Form1();
+                f.propertyGrid1.SelectedObject = this.ApplicationObject.ActiveDocument;
+                f.ShowDialog();
+            }
+            else
+                MessageBox.Show("ActiveDocument IS NULL");
+
+            //ReportParametersUI.MainWindow w = new ReportParametersUI.MainWindow();
+            //w.ShowDialog();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+    }
+
+    
+
+}
